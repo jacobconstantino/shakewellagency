fe876b7c5a9f572321d5a06e850753a6c2ef9846
John Sovereign U. Constantino <constantino.johnsovereign22@gmail.com>
2024-03-08 11:01:38 +0800

    created migration file for users,jobs, and voucher table

--- database/migrations/2024_03_08_025654_create_voucher_table.php
+++ database/migrations/2024_03_08_025654_create_voucher_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('voucher', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('first_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('voucher')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->nullable();  
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('voucher');
     }
 };
